6dec020bb8415115cae31ae851d924eaa3d9d1cd
zuongthao <zuongthao.tn@gmail.com>
2025-05-21 15:32:34 +0700

    transform data from bronze to gold layer

--- Book-Recomendation-System/dbt/models/gold_layer/book.sql
+++ Book-Recomendation-System/dbt/models/gold_layer/book.sql
@@ -4,8 +4,8 @@ SELECT
     id,
     name,
     url,
-    TRIM(REPLACE(SUBSTRING_INDEX(extra_info, '-', 1), 'avg rating', '')) AS avg_rating,
-    TRIM(REPLACE(SUBSTRING_INDEX(SUBSTRING_INDEX(extra_info, '-', 2), '-', -1), 'ratings', '')) AS num_ratings,
-    TRIM(REPLACE(SUBSTRING_INDEX(extra_info, '-', -1), 'published', '')) AS published_year
+    CAST(TRIM(REPLACE(SUBSTRING_INDEX(extra_info, '-', 1), 'avg rating', '')) AS DECIMAL(3,2))  AS avg_rating,
+    CAST(TRIM(REPLACE(REPLACE(SUBSTRING_INDEX(SUBSTRING_INDEX(extra_info, '-', 2), '-', -1), 'ratings', ''), ',', '')) AS UNSIGNED) AS num_ratings,
+    CAST(TRIM(REPLACE(SUBSTRING_INDEX(extra_info, '-', -1), 'published', '')) AS UNSIGNED) AS published_year
 FROM 
     {{ source('book_bronze_layer', 'book') }}
